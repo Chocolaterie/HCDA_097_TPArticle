df156bd8b45f1ef2a32c319ac491ac2828a7de4e
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-10-31 16:17:29 +0100

    Test deploiement

--- src/main/java/fr/eni/tp_article/article/Article.java
+++ src/main/java/fr/eni/tp_article/article/Article.java
@@ -1,15 +1,7 @@
 package fr.eni.tp_article.article;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
 public class Article {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
 	public Long id;
 	
 	public String title;
